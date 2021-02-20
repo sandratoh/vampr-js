30b8ebac7346e622d4b953425849e5c6f8136a62
Sandra To <sandra.to.e@gmail.com>
2021-02-20 21:28:24 +0000

    Updated vampire class to pass test 1 to 3

--- vampire.js
+++ vampire.js
@@ -10,17 +10,25 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
