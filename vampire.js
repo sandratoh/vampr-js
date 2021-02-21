656ea10cedef54eeddbfa250a3093a8af8db6d9a
Sandra To <sandra.to.e@gmail.com>
2021-02-21 02:03:39 +0000

    Removed non working codes and wrote notes to do depth tree traversal in future

--- vampire.js
+++ vampire.js
@@ -67,45 +67,11 @@ class Vampire {
       if (vampire.creator === null || this.creator === null) {
         return vampire.creator === null ? vampire : this;
 
+      // TO DO: use recursion or depth tree traversal to find lowest common ancestor if none is matching
       // return the one that is more senior?? works for test 5 some reason but wrong code probably
-      // } else if (vampire.isMoreSeniorThan(this) || this.isMoreSeniorThan(vampire)) {
-      //   return vampire.isMoreSeniorThan(this) ? vampire.creator : this.creator;
+      } else if (vampire.isMoreSeniorThan(this) || this.isMoreSeniorThan(vampire)) {
+        return vampire.isMoreSeniorThan(this) ? vampire.creator : this.creator;
       
-    
-    } else if (vampire.isMoreSeniorThan(this)) {
-      let seniorCreator = vampire.creator;
-      let oneUpSeniorCreator = seniorCreator.creator;
-
-      let juniorCreator = this.creator;
-      let oneUpJuniorCreator = juniorCreator.creator;
-      // let twoUpCreator = oneUpJuniorCreator.creator;
-
-      if (oneUpSeniorCreator === oneUpJuniorCreator) {
-        return oneUpSeniorCreator;
-      }
-    
-        // if they don't have the same creator, go up in branch until they reach same creator
-      // } else {
-      //   let moreSeniorVampire;
-      //   let moreJuniorVampire;
-
-      //   if (vampire.isMoreSeniorThan(this)) {
-      //     moreSeniorVampire = vampire;
-      //     moreJuniorVampire = this;
-
-      //   } else {
-      //     moreSeniorVampire = this;
-      //     moreJuniorVampire = vampire;
-      //   }
-        
-      //   // let commonCreator;
-      //   let oneHigherCreator = moreJuniorVampire.creator;
-        
-      //   if (moreSeniorVampire.creator === oneHigherCreator.creator) {
-      //     return oneHigherCreator;
-      //   }
-
-        // return commonCreator;
       }
     }
   }
