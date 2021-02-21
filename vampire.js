53eb803a4c26a19c4d610725c33764969dbf4006
Sandra To <sandra.to.e@gmail.com>
2021-02-21 02:02:22 +0000

    Wrote a bunch of nonworking codes for last test

--- vampire.js
+++ vampire.js
@@ -48,10 +48,16 @@ class Vampire {
       return this;
     }
 
+    // if (vampire === this.creator || this === vampire.creator) {
+    //   return vampire.isMoreSeniorThan(this) ? vampire : this;
+    // }
+
+    // if one or the other is the creator
     if (vampire === this.creator || this === vampire.creator) {
-      return vampire.isMoreSeniorThan(this) ? vampire : this;
+      return vampire === this.creator ? vampire : this;
     }
 
+    // if same creator
     if (vampire.creator === this.creator) {
       return this.creator;
 
@@ -61,14 +67,45 @@ class Vampire {
       if (vampire.creator === null || this.creator === null) {
         return vampire.creator === null ? vampire : this;
 
-      // return the one that is more senior
-      } else if (vampire.isMoreSeniorThan(this) || this.isMoreSeniorThan(vampire)) {
-        return vampire.isMoreSeniorThan(this) ? vampire.creator : this.creator;
+      // return the one that is more senior?? works for test 5 some reason but wrong code probably
+      // } else if (vampire.isMoreSeniorThan(this) || this.isMoreSeniorThan(vampire)) {
+      //   return vampire.isMoreSeniorThan(this) ? vampire.creator : this.creator;
+      
+    
+    } else if (vampire.isMoreSeniorThan(this)) {
+      let seniorCreator = vampire.creator;
+      let oneUpSeniorCreator = seniorCreator.creator;
 
+      let juniorCreator = this.creator;
+      let oneUpJuniorCreator = juniorCreator.creator;
+      // let twoUpCreator = oneUpJuniorCreator.creator;
+
+      if (oneUpSeniorCreator === oneUpJuniorCreator) {
+        return oneUpSeniorCreator;
+      }
+    
+        // if they don't have the same creator, go up in branch until they reach same creator
       // } else {
-        
+      //   let moreSeniorVampire;
+      //   let moreJuniorVampire;
 
-      //   return;
+      //   if (vampire.isMoreSeniorThan(this)) {
+      //     moreSeniorVampire = vampire;
+      //     moreJuniorVampire = this;
+
+      //   } else {
+      //     moreSeniorVampire = this;
+      //     moreJuniorVampire = vampire;
+      //   }
+        
+      //   // let commonCreator;
+      //   let oneHigherCreator = moreJuniorVampire.creator;
+        
+      //   if (moreSeniorVampire.creator === oneHigherCreator.creator) {
+      //     return oneHigherCreator;
+      //   }
+
+        // return commonCreator;
       }
     }
   }
